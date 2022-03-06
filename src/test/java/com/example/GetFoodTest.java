7dd635d130fd9d4c84d2071948c93c7905df4136
Alina Timofeeva <alina.timofeeva-2012@yandex.com>
2022-03-07 00:48:02 +0400

    add jacoco report

---
+++ src/test/java/com/example/GetFoodTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class GetFoodTest{
+
+    public void testGetFood() {
+    }
+
+}
---
+++ src/test/java/com/example/LionTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import junit.framework.TestCase;
+public class LionTest extends TestCase {
+
+    public void testGetKittens() {
+    }
+
+    public void testDoesHaveMane() {
+    }
+
+    public void testGetFood() {
+    }
+}
